1974d8f436674c525852f3fa69b1553de951386e
AntonioNvs <tonim2612@gmail.com>
2024-12-09 12:45:52 -0300

    Add more tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,56 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+
+  it('Limpa uma tarefa completa', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[class=clear-completed]')
+      .click();
+  });
+
+  it('Limpa somente as tarefas completas', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[class=clear-completed]')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
+
+  it('Insere cinco tarefas', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+      .type('RP1 de ES{enter}')
+      .type('RP2 de ES{enter}')
+      .type('Estudo da Prova de ES{enter}')
+
+      
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 5)
+  });
 });
